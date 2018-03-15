d1752e12f8d210b57e80c47237c10a73b3341bd0
Alexander Roode <alexroode@gmail.com>
2018-03-14 18:41:03 -0700

    Add bio and home content

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -2,3 +2,4 @@ var shell = require('shelljs');
 
 shell.cp('-R', 'src/public/audio', 'dist/public/');
 shell.cp('-R', 'src/public/scores', 'dist/public/');
+shell.cp('-R', 'src/public/img', 'dist/public/');
