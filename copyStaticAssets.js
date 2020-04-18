b2b31eacf5c85062eb881126ec63730e7d39a4cb
Alexander Roode <alexroode@gmail.com>
2020-04-18 12:07:51 -0700

    Update npm scripts

--- copyStaticAssets.js
+++
@@ -1,6 +0,0 @@
-var shell = require('shelljs');
-
-shell.cp('src/music/music.json', 'dist/music/');
-shell.cp('-R', 'src/public/audio', 'dist/public/');
-shell.cp('-R', 'src/public/scores', 'dist/public/');
-shell.cp('-R', 'src/public/img', 'dist/public/');
