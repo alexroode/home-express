efa63233a8b3b5023d40224f80a76d2cfe1e15af
Alexander Roode <alexroode@gmail.com>
2020-12-21 14:11:01 -0800

    Copy piece descriptions to dist

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
         }),
         new CopyPlugin([
             { from: './src/public/img/*.*', to: './img', flatten: true },
-            { from: './src/music/music.json', to: '../music/', flatten: true }
+            { from: './src/music/music.json', to: '../music/', flatten: true },
+            { from: './src/music/pieces/*.md', to: '../music/pieces/', flatten: true }
         ])
     ],
     module: {
